346723f0b9937e506d82c7b5f746563827e1d5c3
FSantiago1987 <rawpower_br@hotmail.com>
2021-01-11 12:29:57 -0800

    removed add element button from add view, removed next and previous button from list and category view

--- config/app.js
+++ config/app.js
@@ -89,6 +89,23 @@ app.locals.truncate = (str, len) =>  {
   return str;
 }
 
+app.locals.truncate = (str, len) =>  {
+  if(str.length > len && str.length > 0) {
+    let new_str = str + '';
+    new_str = str.substr(0, len);
+    new_str = str.substr(0, new_str.lastIndexOf(' '));
+    new_str = new_str.length > 0 ? new_str: str.substr(0, len);
+    return new_str + '...';
+  }
+  return str;
+}
+
+app.locals.capitalize = (str) =>  {
+  if(typeof str !== 'string')
+    return '';
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 app.locals.Speak = (str) => {
   say.speak(str);
 }
