36ee5487ed7c239cf0f4ea661af26f3610395238
FSantiago1987 <rawpower_br@hotmail.com>
2021-01-14 18:02:08 -0800

    help page added and contact route and view implemented

--- controllers/index.js
+++ controllers/index.js
@@ -5,3 +5,11 @@ module.exports.displayHomePage = (req, res, next) => {
     res.render('index', {title: 'Home', displayName: req.user ? req.user.displayName: ''});
 }
 
+module.exports.displayContactPage = (req, res, next) => {
+    res.render('contact', {title: 'Contact', displayName: req.user ? req.user.displayName: ''});
+}
+
+module.exports.displayHelpPage = (req, res, next) => {
+    res.render('help', {title: 'Help', displayName: req.user ? req.user.displayName: ''});
+}
+
